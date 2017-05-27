d020cc6130eb93b41ac2309202c4d3989a853ac6
Fabian Hagen <fabianhagen87@gmail.com>
2017-05-27 07:11:14 +0200

    German translation file
    Added german translation.

---
+++ resources/lang/de/messages.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'create_success' => 'Erfolg! Neues :entity wurde angelegt.',
+    'update_success' => 'Erfolg! :entity wurde aktualisiert.',
+    'delete_success' => 'Erfolg! :entity wurde gelöscht.',
+    'upload_success' => 'Erfolg! :entity hochgeladen.',
+    'move_success'   => 'Erfolg! Neues :entity wurde verschoben.',
+    'rename_success' => 'Erfolg! Neues :entity wurde umbenannt.',
+
+    'create_error' => 'Fehler! Beim Anlegen von :entity ist ein Fehler aufgetreten.',
+    'delete_error' => 'Fehler! Beim Löschen von :entity ist ein Fehler aufgetreten.',
+    'upload_error' => 'Fehler! Beim Hochladen von :entity ist ein Fehler aufgetreten.',
+    'move_error'   => 'Fehler! Beim Verschieben von :entity ist ein Fehler aufgetreten.',
+    'rename_error' => 'Fehler! Beim Umbenennen von :entity ist ein Fehler aufgetreten.',
+];
