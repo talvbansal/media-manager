a70d566e4d2d8f1eda158a8c25c157b38f4357c2
talvbansal <talvbansal@hotmail.com>
2018-03-19 11:46:22 +0000

    Add comments for the config file

--- src/Config/media-manager.php
+++ src/Config/media-manager.php
@@ -8,15 +8,34 @@
 
 return [
 
+    /**
+     * Change the bread crumb behaviour.
+     * Here you can configure the text output for the root folder of the media-manager...
+     */
     'breadcrumb' => [
         'root' => 'Files',
     ],
 
+    /**
+     * Change the route behaviour.
+     * Here you can apply additional middleware to be applied to media-manager routes
+     * as well as setting a custom path prefix for the media-manager routes to be
+     * accessible from...
+     */
     'routes' => [
         'middleware' => [],
         'prefix'     => env('MEDIA_MANAGER_ROUTE_PREFIX', '/admin/'),
     ],
 
+    /**
+     * Set the disk for the media manager to use, this can be one of the disks defined
+     * within your projects `config/filesystems.php` file...
+     */
     'disk'   => env('MEDIA_MANAGER_STORAGE_DISK', 'public'),
+
+    /**
+     * Configure the Access Mode of the uploaded files.
+     * By default S3 uploads are private, we're setting them to public here.
+     */
     'access' => env('MEDIA_MANAGER_ACCESS', 'public'),
 ];
