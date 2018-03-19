37936444d356e41aab395c402c5837e957f241b0
talvbansal <talvbansal@hotmail.com>
2018-03-19 12:33:16 +0000

    Strip out excess locales from moment js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,23 @@
+const webpack = require('webpack');
 let mix = require('laravel-mix');
 
+/*
+ |--------------------------------------------------------------------------
+ | Custom Mix setup
+ |--------------------------------------------------------------------------
+ |
+ */
+
+mix.webpackConfig({
+    plugins: [
+        new webpack.ContextReplacementPlugin(
+            // The path to directory which should be handled by this plugin
+            /moment[\/\\]locale/,
+            // A regular expression matching files that should be included
+            /(en-gb)\.js/
+        )
+    ]
+});
 mix.sass('resources/assets/sass/media-manager.scss', 'public/css/')
     .js('resources/assets/js/media-manager.js', 'public/js/')
     .copy('resources/assets/fonts', 'public/fonts/')
