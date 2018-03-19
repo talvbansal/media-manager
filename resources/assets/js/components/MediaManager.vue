a3ce220623bce9300bde45440e0f6daf32a5f5fc
Talv <talvbansal@hotmail.com>
2018-03-19 00:02:56 +0000

    Extract preview sidebar into its own component

--- resources/assets/js/components/MediaManager.vue
+++ resources/assets/js/components/MediaManager.vue
@@ -139,52 +139,7 @@
                   </table>
                 </div>
               </div>
-              <div 
-                v-if="isFile(currentFile) && !isFolder(currentFile)"
-                class="easel-file-picker-sidebar hidden-xs col-sm-3">
-
-                <img 
-                  v-if="isImage(currentFile)"
-                  id="easel-preview-image"
-                  :src="currentFile.webPath"
-                  class="img-responsive center-block"
-                  style="max-height: 200px"
-                >
-
-                <div 
-                  v-else 
-                  class="text-center">
-                  <i 
-                    class="icon-file-text2" 
-                    style="font-size: 15rem"/>
-                </div>
-
-                <table class="table-responsive table-condensed table-vmiddle easel-file-picker-preview-table">
-                  <tbody>
-                    <tr>
-                      <td class="description">Name</td>
-                      <td class="file-value">{{ currentFile.name }}</td>
-                    </tr>
-                    <tr>
-                      <td class="description">Size</td>
-                      <td class="file-value">{{ currentFile.size | humanFileSize }}</td>
-                    </tr>
-                    <tr>
-                      <td class="description">URL</td>
-                      <td class="file-value">
-                        <a 
-                          :href="currentFile.webPath" 
-                          target="_blank" 
-                          rel="noopener">{{ currentFile.relativePath }}</a>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td class="description">Uploaded On</td>
-                      <td class="file-value">{{ currentFile.modified.date | moment }}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
+              <preview-sidebar :current-file="currentFile"/>
             </div>
           </div>
         </div>
@@ -267,6 +222,7 @@ export default {
 		"modal-create-folder": require("./subcomponents/CreateFolderModal.vue"),
 		"modal-delete-item": require("./subcomponents/ConfirmDeleteModal.vue"),
 		"modal-move-item": require("./subcomponents/MoveItemModal.vue"),
+		"preview-sidebar": require("./subcomponents/PreviewSideBar.vue"),
 		"top-toolbar": require("./subcomponents/TopToolBar.vue"),
 	},
 
---
+++ resources/assets/js/components/subcomponents/PreviewSideBar.vue
@@ -0,0 +1,61 @@
+<template>
+  <div
+    v-if="isFile(currentFile) && !isFolder(currentFile)"
+    class="easel-file-picker-sidebar hidden-xs col-sm-3">
+
+    <img
+      v-if="isImage(currentFile)"
+      id="easel-preview-image"
+      :src="currentFile.webPath"
+      class="img-responsive center-block"
+      style="max-height: 200px"
+    >
+
+    <div
+      v-else
+      class="text-center">
+      <i
+        class="icon-file-text2"
+        style="font-size: 15rem"/>
+    </div>
+
+    <table class="table-responsive table-condensed table-vmiddle easel-file-picker-preview-table">
+      <tbody>
+        <tr>
+          <td class="description">Name</td>
+          <td class="file-value">{{ currentFile.name }}</td>
+        </tr>
+        <tr>
+          <td class="description">Size</td>
+          <td class="file-value">{{ currentFile.size | humanFileSize }}</td>
+        </tr>
+        <tr>
+          <td class="description">URL</td>
+          <td class="file-value">
+            <a
+              :href="currentFile.webPath"
+              target="_blank"
+              rel="noopener">{{ currentFile.relativePath }}</a>
+          </td>
+        </tr>
+        <tr>
+          <td class="description">Uploaded On</td>
+          <td class="file-value">{{ currentFile.modified.date | moment }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+	props:{
+		currentFile : {
+			default: function(){
+				return {};
+			},
+			type: [Object, Boolean]
+		},
+	}
+};
+</script>
